fca314578a89d3cec863807ac2d52fdb77b366aa
Colino <colcmorris@gmail.com>
2021-06-04 23:58:40 +0100

    More improvements to Finance_apr with LocalDate use instead of Calendar object

--- my/custom/finance/Finance_apr.java
+++ my/custom/finance/Finance_apr.java
@@ -8,8 +8,9 @@ package my.custom.finance;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.util.Calendar;
+import java.util.*;
 import java.util.regex.Pattern;
+import java.time.*;
 
 /**
  *
@@ -32,8 +33,10 @@ public class Finance_apr
     private String date_from;
     private String date_to;
     
-    private Calendar calendar_date_from;
-    private Calendar calendar_date_to;
+    private LocalDate calendar_date_from;
+    //private Calendar calendar_date_from;
+    private LocalDate calendar_date_to;
+    //private Calendar calendar_date_to;
     
     //public Finance_apr(double month_repayment, double mort_remain, double apr_int_rate)
     public Finance_apr()
@@ -43,54 +46,56 @@ public class Finance_apr
     
     public void processMortgateInterestCalculation()
     {
-       this.calendar_date_from = Calendar.getInstance();
-       this.calendar_date_to = Calendar.getInstance();
-        
-//        this.setMonthRepayment(month_repayment);
-//        this.setInterestRate(apr_int_rate);
-//        this.setMortgageRemaining(mort_remain);
-        
-//        SimpleDateFormat future_date = new SimpleDateFormat ("yyyy-MM-dd");
-//        calendar_date_to.;
-        this.calendar_date_to.set(2022, 2,31);
-        int date_2_day_of_week = calendar_date_to.get(Calendar.DAY_OF_WEEK); // 5 = Thursday, 0 = Saturday, etc.
+//        this.calendar_date_from = Calendar.getInstance();
+//        this.calendar_date_to = Calendar.getInstance();
+
+        this.calendar_date_from = LocalDate.now();
+        this.calendar_date_to=  LocalDate.parse("2022-04-01");
+        this.setDateRanges();
+        //int date_2_day_of_week = calendar_date_to.get(Calendar.DAY_OF_WEEK); // 5 = Thursday, 0 = Saturday, etc.
+        //String date_2_day_of_week = calendar_date_to.getDayOfWeek().toString(); // 5 = Thursday, 0 = Saturday, etc.
        
         SimpleDateFormat ft = new SimpleDateFormat ("E yyyy.MM.dd 'at' hh:mm:ss a zzz");        
-        String date1_string = ft.format(calendar_date_from.getTime());
+        //String date1_string = ft.format(calendar_date_from.toString());
+        //String date1_string = this.calendar_date_from .toString();
 
-        String date2_string = ft.format(calendar_date_to.getTime());
+        //String date2_string = ft.format(calendar_date_to.toString());
+        //String date2_string =this.calendar_date_to.toString();
 
-        long diff = calendar_date_to.getTimeInMillis() - calendar_date_from.getTimeInMillis();
+        //long diff = calendar_date_to.getTimeInMillis() - calendar_date_from.getTimeInMillis();
+        //long diff = calendar_date_to. - calendar_date_from.;
 
-        float dayCount = (float) diff / (24 * 60 * 60 * 1000);
+        //float dayCount = (float) diff / (24 * 60 * 60 * 1000);
+        float dayCount = Duration.between(this.calendar_date_from.atStartOfDay(), this.calendar_date_to.atStartOfDay()).toDays();
         System.out.println("** Calculations are based on a monthly repayment of £" + month_repayment + " **");
-        System.out.println("== Calculating the days between " + date1_string + " and " + date2_string + "("+ date_2_day_of_week +") ==");
+        //System.out.println("== Calculating the days between " + date1_string + " and " + date2_string + "("+ date_2_day_of_week +") ==");
         System.out.println("== dayCount = " + dayCount);
         //Initializing the date formatter
         DateFormat Date = DateFormat.getDateInstance();
         //Initializing Calender Object
-        Calendar cals = Calendar.getInstance();
+        //Calendar cals = Calendar.getInstance();
         //Displaying the actual date
-        System.out.println("The original Date: " + cals.getTime());
+        System.out.println("The original Date: " + this.calendar_date_from.toString());
         //Using format() method for conversion
-        String currentDate = Date.format(cals.getTime());
-        System.out.println("Formatted Date: " + currentDate);
+        //String currentDate = Date.format(cals.getTime());
+        //System.out.println("Formatted Date: " + currentDate);
         
         System.out.println("== Lets do a Local date addition ==");
         SimpleDateFormat today_date = new SimpleDateFormat ("yyyy-MM-dd");
-        LocalDate date = LocalDate.parse(today_date.format(calendar_date_from.getTime()));
+        //LocalDate date = LocalDate.parse(today_date.format(calendar_date_from.getTime()));
+        LocalDate date = this.calendar_date_from;
 
         double mort_remain_new;
         //double apr_int_rate = 1.64;
         //double day_int_rate = (this.interest_rate / 365);
         //double day_int_charge;
 
-        Float f = dayCount;
-        int days_count = f.intValue();
+        //Float f = dayCount;
+        //int days_count = (int)dayCount;
         //int days_count = java.;
-        LocalDate date_add = date.plusDays(days_count);
+        LocalDate date_add = date.plusDays((int)dayCount);
         LocalDate date_add_single;
-        for(int i = 0; i <= days_count; i++)
+        for(int i = 0; i <= (int)dayCount; i++)
         {
             date_add_single = date.plusDays(i);
             if(i != 0 && date_add_single.getDayOfMonth() == 1)
@@ -104,14 +109,11 @@ public class Finance_apr
             System.out.println(i +") On date "+ date_add_single +" the mortgate remaining is " + String.format("%.2f",this.mortgage_remaining) + " and for apr: " + this.interest_rate + ", the daily interest charge is "+ String.format("%.2f",this.day_int_charge));
             
             this.mortgage_remaining += this.day_int_charge;
-            
-            
-            
-            //mort_remain = 23274.67;
+
         }
         System.out.println("** Calculations were based on a monthly repayment of £" + month_repayment + " **");
         System.out.println("== Final amount of days: ==");
-        System.out.println("Date "+date+" plus " + days_count + " days is "+date_add);          
+        System.out.println("Date " + date + " plus " + (int)dayCount + " days is "+date_add);          
     }
     
     // * Validation | START //
@@ -146,12 +148,43 @@ public class Finance_apr
             
     public void setDateToCalculateTo(String dt)
     {
-        this.date_from = dt;
+        this.date_to = dt;
     }
     
     public String getDateToCalculateTo()
     {
         return this.date_to;
+    }
+    
+    private void setDateRanges()
+    {
+        String date_set1[] = this.date_from.split("-");
+        //System.out.println(date_set[0] + "," + (Integer.valueOf(date_set[1]) -1) + "," + date_set[0]);
+        if(date_set1.length == 3)
+        {         
+            this.calendar_date_from = LocalDate.parse(date_from);            
+        }
+        else
+        {
+            System.out.println("Note: default start date to: " + this.calendar_date_from.toString());
+        }
+
+        
+        
+        String date_set2[] = this.date_to.split("-");
+        if(date_set1.length == 3)
+        {
+            int year = Integer.valueOf(date_set2[0]);
+            int month200 = (Integer.valueOf(date_set2[1]) -1);
+            int day = Integer.valueOf(date_set2[2]);
+            //this.calendar_date_to.of(year, month, day);            
+            this.calendar_date_to = LocalDate.parse(date_to);            
+        }
+        else
+        {
+            this.calendar_date_to = this.calendar_date_from.plusMonths(6);
+            System.out.println("Note: Setting default end date to: " + this.calendar_date_to.toString());
+        }
     }
   
     /**
